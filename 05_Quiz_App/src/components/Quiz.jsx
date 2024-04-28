39d8a5fbcb89ce2652d3d3b390088ad0d34c66c9
Shekhar <Shekhar209203@gmail.com>
2024-04-28 16:14:31 +0530

    Finished Quiz App

--- 05_Quiz_App/src/components/Quiz.jsx
+++ 05_Quiz_App/src/components/Quiz.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { QuestionData } from "../QuestionData";
+import QuizResult from "./QuizResult";
 function Quiz() {
   const [currentQues, setCurrentQues] = useState(0);
   const [score, setScore] = useState(0);
   const [clickedOption, setClickedOption] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
   const changeQuestion = () => {
     updateScore();
     if (currentQues < QuestionData.length - 1) {
       setCurrentQues(currentQues + 1);
+      setClickedOption(0);
+    } else {
+      setShowResult(true);
     }
   };
   const updateScore = () => {
@@ -15,36 +21,54 @@ function Quiz() {
       setScore(score + 1);
     }
   };
+  const resetAll = () => {
+    setShowResult(false);
+    setCurrentQues(0);
+    setClickedOption(0);
+    setScore(0);
+  };
   return (
     <div>
       <p className="heading-txt">Quiz App</p>
       <div className="container">
-        <div className="question">
-          <span id="question-number">{currentQues + 1}.</span>
-          <span id="question-txt">{QuestionData[currentQues].question}</span>
-        </div>
-        <div className="option-container">
-          {QuestionData[currentQues].options.map((option, i) => {
-            return (
-              <button
-                className={`option-btn ${
-                  clickedOption == i + 1 ? "checked" : null
-                }`}
-                //className="option-btn"
-                key={i}
-                onClick={() => setClickedOption(i + 1)}
-              >
-                {option}
-              </button>
-            );
-          })}
-        </div>
-        <input
-          type="button"
-          value="Next"
-          id="next-button"
-          onClick={changeQuestion}
-        />
+        {showResult ? (
+          <QuizResult
+            score={score}
+            totalScore={QuestionData.length}
+            tryAgain={resetAll}
+          />
+        ) : (
+          <>
+            <div className="question">
+              <span id="question-number">{currentQues + 1}.</span>
+              <span id="question-txt">
+                {QuestionData[currentQues].question}
+              </span>
+            </div>
+            <div className="option-container">
+              {QuestionData[currentQues].options.map((option, i) => {
+                return (
+                  <button
+                    className={`option-btn ${
+                      clickedOption == i + 1 ? "checked" : null
+                    }`}
+                    //className="option-btn"
+                    key={i}
+                    onClick={() => setClickedOption(i + 1)}
+                  >
+                    {option}
+                  </button>
+                );
+              })}
+            </div>
+            <input
+              type="button"
+              value="Next"
+              id="next-button"
+              onClick={changeQuestion}
+            />
+          </>
+        )}
       </div>
     </div>
   );
